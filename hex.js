bb506473d97211b6f0048fefa3cf2d02a9968e17
Milad Behzadi <milad.bhzdi@gmail.com>
2021-03-31 12:41:23 +0200

    add comment on hex.js and adding styles.css

--- hex.js
+++ hex.js
@@ -7,14 +7,27 @@ const color = document.querySelector('.color')
 
 btn.addEventListener('click', function(){
 
-//if I fix const and not let I'am going to have an error
+//Je choisis let et non const pour que je puisse changer la valeur de hexColor.
 
 let hexColor = '#'
 
+console.log(hex[getRandomNumber()]);
+
 for( let i = 0 ; i < 6; i++) {
-    hexColor += hex[0] 
+    // Je fais une ittération allant de 0 à 6 car il me faut
+    hexColor += hex[getRandomNumber()] 
 }
 color.textContent = hexColor
 document.body.style.backgroundColor = hexColor
 
 })
+
+// Je crée une fonction expression qui va générer des valeurs au hasard.
+// Je fix à 16 ou à la longueur du tableau hex.
+// Je génére un nombre flottant généré entre 0 (inclus) et 1 (exclu).
+// La fonction Math.floor(x) renvoie le plus grand entier qui est inférieur ou égal à un nombre x.
+// Donc j'aurais des nombres au hasard allant de 0 à 16.
+// Par la suite je passe ma fonction dans l'index du tableau hex.
+// Et a chaque ittération grâce à ma boucle for j'incrémente hexColor.
+
+const getRandomNumber = () => Math.floor(Math.random() * hex.length)
